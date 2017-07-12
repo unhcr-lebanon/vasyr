ec4e30d052115c0c0997fe68dac8e96b174f73af
Edouard-Legoupil <edouard.legoupil@gmail.com>
2017-07-12 18:47:43 +0300

    add weighting

--- code/1-b.clean-data.R
+++ code/1-b.clean-data.R
@@ -2,7 +2,7 @@
 ### Clean and weight data         ####
 #######################################
 
-source("code/1-a.loaddata.R")
+source("code/1-a.load-data.R")
 
 ################################################################
 #### Check location
@@ -57,10 +57,53 @@ names(Correct.form)
 ################################################################
 #### Weighting data
 
-weight <- read_excel("data/weight data 22_06-2017-2.xlsx",  sheet = " weight2206")
-location.vasyr.district <- as.data.frame(unique(household2$section1.location.location))
+weight <- read_excel("data/weight22_06-2017-2.xlsx",  sheet = "weight2206")
 
-names(location.vasyr.district)[1] <- "Districts"
+## Check the merge on location
+#location.vasyr.district <- as.data.frame(unique(household$section1.location.district))
+
+#names(location.vasyr.district)[1] <- "Districts"
+#location.vasyr.district$rowid <- row.names(location.vasyr.district)
 #location.vasyr.district <- as.data.frame(location.vasyr.district[!(is.na(location.vasyr.district$Districts )), ])
-names(weight)
-weight <- merge(x=weight, y=location.vasyr.district, by="Districts", all.x=TRUE)
+#names(weight)
+#location.vasyr.district$Districts <- as.character(location.vasyr.district$Districts)
+#write.csv(location.vasyr.district, "data/locationvasyrdistrict.csv")
+#str(location.vasyr.district$Districts)
+#str(weight$Districts)
+#weight <- merge(x=weight, y=location.vasyr.district, by="Districts", all.x=TRUE)
+
+
+## Cf https://rpubs.com/trjohns/survey-cluster
+## caluclate fpc i.e the number of clusters
+fpc <- nrow(weight)
+weight$fpc <- fpc
+## Good -- let's trim the weight frame
+
+weight2 <- weight[ c("Districts",   "Normalized.Weight","fpc")]
+names(weight2)[1] <- "section1.location.district"
+household <- join(x=household, y=weight2, by="section1.location.district")
+#household$rowid <- row.names(household)
+
+#table(household$Normalized.Weight)
+prop.table(table(household$section1.location.district, useNA="ifany"))
+## We have some NA for the district -- needs to exclude them
+
+household2 <- household[ !(is.na(household$section1.location.district)), ]
+
+## Now testing weighting using the survey library
+library(survey)
+
+household.survey <- svydesign(ids = ~ section1.location.district ,  data = household2 ,  weights = ~Normalized.Weight ,  fpc = ~fpc )
+summary(household.survey)
+svymean(~ section3_household.housing.type_of_housing, design = household.survey)
+
+#frequ <- as.data.frame(prop.table(table(household$section3_household.housing.type_of_housing, household$section1.location.district )))
+frequ <- as.data.frame(prop.table(table(household$section3_household.housing.type_of_housing )))
+
+#frequ.weight <- as.data.frame( svyby(  ~ section3_household.housing.type_of_housing, by = ~ section1.location.district, design = household.survey, FUN = svymean  ))
+frequ.weight <- as.data.frame(svymean(~ section3_household.housing.type_of_housing, design = household.survey))
+frequ.weight$Var0 <- row.names(frequ.weight)
+frequ.weight$Var1 <- substr(frequ.weight$Var0, regexpr("section3_household.housing.type_of_housing",frequ.weight$Var0), 10)
+
+frequ.weight$Var1 <- substr(as.character(frequ.weight$Var0), 42, nchar(frequ.weight$Var0)-42)
+nchar(frequ.weight$Var0)
